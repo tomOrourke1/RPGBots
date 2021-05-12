a17d351c14483f93e5eb94d763f53a6c8c112758
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2021-05-12 10:55:37 -0400

    Added Third Person Mover

---
+++ Assets/Scripts/ThirdPersonMover.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMover : MonoBehaviour
+{
+    [SerializeField] float _turnSpeed = 1000f;
+    [SerializeField] float _moveSpeed = 5f;
+    
+    Rigidbody _rigidbody;
+
+    void Awake() => _rigidbody = GetComponent<Rigidbody>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseMovement = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseMovement * Time.deltaTime * _turnSpeed, 0);
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        var velocity = new Vector3(horizontal, 0, vertical);
+        velocity *= _moveSpeed * Time.fixedDeltaTime;
+
+        Vector3 offset = transform.rotation * velocity;
+        _rigidbody.MovePosition(transform.position + offset);
+        
+    }
+}
