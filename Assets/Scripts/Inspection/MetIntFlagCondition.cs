7dae84055054408c3f49f12ff1847dc7aee3794f
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2021-06-17 12:57:50 -0400

    added met int flag condition

---
+++ Assets/Scripts/Inspection/MetIntFlagCondition.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MetIntFlagCondition : MonoBehaviour, IMet
+{
+    [SerializeField] private IntGameFlag _requiredFlag;
+    [SerializeField] private int _requiredValue = 2;
+
+    public bool Met() => _requiredFlag.Value >= _requiredValue;
+}
